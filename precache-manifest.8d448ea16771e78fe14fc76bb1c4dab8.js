744f1b85ebe5640067fefca5c30288564ed5131d
Gabriel Radu <radu.gabriel131@gmail.com>
2020-10-04 11:41:07 +0300

    Updates

---
+++ precache-manifest.8d448ea16771e78fe14fc76bb1c4dab8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf2fa54055cc5e5f5f8b4a9f671cb9d",
+    "url": "/cv-project/index.html"
+  },
+  {
+    "revision": "25892db0e6b0381420ab",
+    "url": "/cv-project/static/js/2.2c46ec33.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cv-project/static/js/2.2c46ec33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e380a3022faf7a7edd3",
+    "url": "/cv-project/static/js/main.ab0224e6.chunk.js"
+  },
+  {
+    "revision": "f1ebb3293288666c5950",
+    "url": "/cv-project/static/js/runtime-main.d91c792d.js"
+  }
+]);
